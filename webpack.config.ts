5b201bb0e0230dd8935154e5b4dae21ef718b2bf
Dmitry Ovsienko <ovsienkodima@mail.ru>
2023-02-18 12:01:50 +0300

    the project is configured

---
+++ webpack.config.ts
@@ -0,0 +1,62 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import webpack from 'webpack';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './index.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // изображения
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // CSS, PostCSS, Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'], /* MiniCssExtractPlugin вместо style-loader для продакшена */
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 4000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './index.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
